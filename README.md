b7222ba9f5968d114049a14af4410b68945e3fcf
gdkbtm <gdkbtm@mailcity.com>
2025-09-06 17:55:24 -0400

    clean up

--- README.md
+++ README.md
@@ -4,23 +4,16 @@
 - Designed app layout and user interface using Streamlit
 
 ## Code and Resources Used
-**Python Version:** 3.11 <br>
+**Python Version:** 3.13 <br>
 **Packages:** pandas, numpy, sklearn, streamlit <br>
 **Data Source:** https://www.kaggle.com/datasets/saurabhshahane/spotgen-music-dataset <br>
 **Web Framework Requirements:** ``pip install -r requirements.txt`` <br>
 **Streamlit Documentation**: https://docs.streamlit.io/ <br>
 
-## Check out the app here
-https://music-for-you.streamlit.app/
+ #The Elbow Point: Optimal k Value for music search
+ #Check spotify_music_ellow.py
 
-## Check out the demo video 
-https://youtu.be/5OmFSvSPjZA
-
-## Detailed blog post
-https://hahoangpro.wixsite.com/datascience/post/song-recommendation-app-using-streamlit-and-k-nearest-neighbors
-
-## Additional projects
-https://hahoangpro.wixsite.com/datascience/blog
+ 
 
 
 
--- predict_music_popularity.py
+++ predict_music_popularity.py
@@ -1,8 +1,8 @@
 #   
 # run Music-Recommend-Spotify % python predict_music_popularity.py --genre 'pop'
 # run Music-Recommend-Spotify % python predict_music_popularity.py (default genre is rock)
-
-
+# creates artist_recommend.csv based on genre provided
+ 
 # Import libraries
  
 import glob
@@ -21,8 +21,6 @@ import argparse
 
 def load_data():
     all_files = glob.glob("music_data/*.csv")
-    #df = pd.read_csv("data/filtered_track_df.csv")
-    #df = pd.read_csv("data/filtered_track_classic_df.csv")
 
     li = []
 
@@ -33,15 +31,13 @@ def load_data():
     df = pd.concat(li, axis=0, ignore_index=True)
     #count total rows
     row_count = len(df)
-    #print('ZZZZZZZ ', row_count)
     #remove duplicates based on uri value
     df = df.drop_duplicates(subset=['uri'], keep='first')
     row_count = len(df)
-    #print(row_count)
-    #df = pd.read_csv(filenames)
+   
     df['genres'] = df.genres.apply(lambda x: [i[1:-1] for i in str(x)[1:-1].split(", ")])
     exploded_track_df = df.explode("genres")
-    #print(len(df))
+  
     filtered_df = df
     return exploded_track_df, filtered_df
 
@@ -54,13 +50,10 @@ audio_feats = ["acousticness", "danceability", "energy", "instrumentalness", "va
 #exploded_track_df, filtered_df = load_data()
 
 # Define function to return Spotify URIs and audio feature values of top neighbors (ascending)
-def n_neighbors_uri_audio(exploded_track_df, filtered_df, genre):
-    #genre = 'folk rock'
+def n_neighbors_uri_audio(exploded_track_df, filtered_df, genre):    
     start_year = 1960
     end_year = 2000
     genre = genre.lower()
-    #print(exploded_track_df["genres"])
-    #print(exploded_track_df['artists_id'], exploded_track_df['artists_name'])
     genre_data = exploded_track_df[(exploded_track_df["genres"]==genre) & (exploded_track_df["release_year"]>=start_year) & (exploded_track_df["release_year"]<=end_year)]
     
     genre_data = genre_data.sort_values(by='popularity', ascending=False)[:500] # use only top 500 most popular songs
@@ -77,44 +70,17 @@ def n_neighbors_uri_audio(exploded_track_df, filtered_df, genre):
     test_feat = [acousticness, danceability, energy, instrumentalness, valence, tempo]
     #test_feat = [0.5, 0.5, 0.5, 0.0, 0.45, 118.0]
     
-    #n_neighbors = neigh.kneighbors([test_feat], n_neighbors=len(genre_data), return_distance=False)[0]
     n_neighbors = neigh.kneighbors([test_feat], n_neighbors=len(genre_data), return_distance=False)[0]
     
-    #artists_id = exploded_track_df['artists_id']
     artists_id = genre_data.iloc[n_neighbors]['artists_id'].to_numpy()
     artists_name = genre_data.iloc[n_neighbors]['artists_name'].to_numpy()
-    artist_info = [genre_data.iloc[n_neighbors]['artists_id'].to_numpy(), genre_data.iloc[n_neighbors]['artists_name'].to_numpy()]
     uris = genre_data.iloc[n_neighbors]["uri"].tolist()
     audios = genre_data.iloc[n_neighbors][audio_feats].to_numpy()
-    #print(artists_name[0])
-    print(artist_info)
-
+  
     return genre_data
-#genre_data = n_neighbors_uri_audio(exploded_track_df, filtered_df)
-
-def dataset_parquet_file(genre_data):
-    temp_folder_path = '/Users/dineshk/work/MLOps/music/Music-Recommend-Spotify/music_data'
-    
-    genre_data.rename(columns=lambda x: re.sub(r'[^\w]', '', x), inplace=True)
-    # Shuffling the dataset for more accurate training and testing results.
-    genre_data = genre_data.sample(frac=1).reset_index(drop=True)
-    # Splitting the data up so that 80% of the data is training data, 20% testing data.
-    training_data = genre_data[:int(genre_data.shape[0]*.8)]
-    testing_data = genre_data[int(genre_data.shape[0]*.8):]
-    
-    print("Creating diamonds dataset parquet files...")
-    with open(os.path.join(temp_folder_path, "train_artists.parquet"), 'w'):
-        training_data.to_parquet(os.path.join(temp_folder_path, "train_artists.parquet"))
-    with open(os.path.join(temp_folder_path, "test_artists.parquet"), 'w'):
-        testing_data.to_parquet(os.path.join(temp_folder_path, "test_artists.parquet"))
-    
-    print("Artists dataset parquet files created.")
-
-    return genre_data   
-#genre_data = dataset_parquet_file(genre_data)
 
 def create_artist_recommend(genre_data):
-    temp_folder_path = '/Users/dineshk/work/MLOps/music/Music-Recommend-Spotify/music_data'
+    temp_folder_path = 'music_data'
 
     # Saving a CSV file of the dataset for predicting purposes.
     #print(genre_data)
@@ -142,5 +108,4 @@ if __name__ == '__main__':
     # Load data
     exploded_track_df, filtered_df = load_data()
     genre_data = n_neighbors_uri_audio(exploded_track_df, filtered_df, parsed_args.genre)
-    genre_data = dataset_parquet_file(genre_data)
     create_artist_recommend(genre_data)
